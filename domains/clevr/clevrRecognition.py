62cdd2c85b16b4c31268a5a59b706fbf15f5800e
Catherine Wong <wong.catherine.mail@gmail.com>
2022-05-19 14:42:29 -0400

    Change parser to accept task names

--- domains/clevr/clevrRecognition.py
+++ domains/clevr/clevrRecognition.py
@@ -18,6 +18,7 @@ from dreamcoder.recognition import RecurrentFeatureExtractor
 from dreamcoder.type import *
 from dreamcoder.domains.clevr.clevrPrimitives import *
 
+
 class ClevrFeatureExtractor(RecurrentFeatureExtractor):
     """
     Domain-specific recognition model for the CLEVR scene tasks.
@@ -26,32 +27,40 @@ class ClevrFeatureExtractor(RecurrentFeatureExtractor):
     tasks: a list of all of the tasks. This is used to extract all of their I/O examples, which we will use to execute Helmholtz programs later on.
     
     """
-    special = 'clevr' # Special flag passed to the OCaml Helmholtz enumerator.
-    serialize_special = makeClevrTasks.serialize_clevr_object # Special flag passed to the OCaml Helmholtz enumerator.
-    maximum_helmholtz = 5000  # Special flag passed to cap how many Helmholtz we enumerate.
-    
+
+    special = "clevr"  # Special flag passed to the OCaml Helmholtz enumerator.
+    serialize_special = (
+        makeClevrTasks.serialize_clevr_object
+    )  # Special flag passed to the OCaml Helmholtz enumerator.
+    maximum_helmholtz = (
+        5000  # Special flag passed to cap how many Helmholtz we enumerate.
+    )
+
     def __init__(self, tasks, testingTasks=[], cuda=False):
         self.recomputeTasks = True
-        self.useTask = True # This means that the tokenize function is called on a Task object, not just examples.
+        self.useTask = True  # This means that the tokenize function is called on a Task object, not just examples.
         self.max_examples = 2
         self.max_examples_length = 200
-        
+
         # Lexicon for all tokens we can ever see in the I/O examples.
         lexicon = clevr_lexicon() + ["OBJ_START", "OBJ_END"]
-        super(ClevrFeatureExtractor, self).__init__(lexicon=lexicon,
-                                                      H=64,
-                                                      tasks=tasks, \
-                                                      bidirectional=True,
-                                                      cuda=cuda,
-                                                      helmholtzTimeout=0.5,
-                                                      helmholtzEvaluationTimeout=0.25)
-                                                      
+        super(ClevrFeatureExtractor, self).__init__(
+            lexicon=lexicon,
+            H=64,
+            tasks=tasks,
+            bidirectional=True,
+            cuda=cuda,
+            helmholtzTimeout=0.5,
+            helmholtzEvaluationTimeout=0.25,
+        )
+
     def tokenize(self, task):
         """Tokenizes the I/O examples of a single task. In particular, a task contains examples in the form (xs, y) where (xs) is a single list of objects, and y is either an attribute, or itself a list of objects.
         
         """
         tokenized = []
         return_type, _ = makeClevrTasks.infer_return_type([task.examples[0][-1]])
+
         def tokenize_obj_list(obj_list):
             flattened = []
             for o in obj_list:
@@ -59,30 +68,42 @@ class ClevrFeatureExtractor(RecurrentFeatureExtractor):
                 for k, v in o.items():
                     if k in ["id", "color", "shape", "size", "material"]:
                         o_attrs += [k, str(v)]
-                    else: # Relations
+                    else:  # Relations
                         o_attrs += [k] + [str(rel) for rel in v]
                 flattened += ["OBJ_START"] + o_attrs + ["OBJ_END"]
             return flattened
+
         def blank_task():
             blank_xs = (["OBJ_START", "OBJ_END"],)
             blank_y = ["OBJ_START", "OBJ_END"]
             return (blank_xs, blank_y)
-        
+
         def example_len(xs, y):
             return sum([len(x) for x in xs]) + len(y)
-        
+
         # To limit recognition backward pass times, we cap the length of these inputs
         for xs, y in task.examples:
             xs = [tokenize_obj_list(obj_list) for obj_list in xs]
-            if return_type in [tint, tbool, tclevrsize, tclevrcolor, tclevrshape, tclevrsize, tclevrmaterial]:
+            if return_type in [
+                tint,
+                tbool,
+                tclevrsize,
+                tclevrcolor,
+                tclevrshape,
+                tclevrsize,
+                tclevrmaterial,
+            ]:
                 y = [str(y)]
             else:
                 y = tokenize_obj_list(y)
             tokenized.append([xs, y])
-        sorted_tokenized = sorted(tokenized, key = lambda e: example_len(e[0], e[1]))
-        
-        sorted_tokenized = [e if example_len(e[0], e[1]) <= self.max_examples_length else blank_task() for e in sorted_tokenized]
-        tokenized = sorted_tokenized[:self.max_examples]
+        sorted_tokenized = sorted(tokenized, key=lambda e: example_len(e[0], e[1]))
+
+        sorted_tokenized = [
+            e if example_len(e[0], e[1]) <= self.max_examples_length else blank_task()
+            for e in sorted_tokenized
+        ]
+        tokenized = sorted_tokenized[: self.max_examples]
         return tokenized
 
     def taskOfProgram(self, p, tp):
@@ -90,7 +111,7 @@ class ClevrFeatureExtractor(RecurrentFeatureExtractor):
         t = super(ClevrFeatureExtractor, self).taskOfProgram(p, tp)
         if t is not None:
             xs, y = t.examples[0]
-            if type(y) == list: # Sort and dedup any object list
+            if type(y) == list:  # Sort and dedup any object list
                 t.examples = [(xs, sort_and_dedup_obj_list(y)) for xs, y in t.examples]
-            t.examples = t.examples[:self.max_examples]
+            t.examples = t.examples[: self.max_examples]
         return t
